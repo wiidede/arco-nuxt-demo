433ad7e363756e69a5ab76e3d89d4ab9a60bd03d
wiidede <wiixdede@gmail.com>
2024-12-26 20:57:39 +0800

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,14 @@ export default defineNuxtConfig({
   modules: [
     'arco-design-nuxt-module',
   ],
+
   arco: {
     importPrefix: 'A',
     hookPrefix: 'Arco',
     locales: ['getLocale'],
     localePrefix: 'Arco',
   },
+
   devtools: { enabled: true },
+  compatibilityDate: '2024-12-26',
 })
